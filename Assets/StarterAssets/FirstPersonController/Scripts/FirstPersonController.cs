1d56c7307cef66e9ab1e6dae5f4506f34386c2cd
TheAsh05 <rashleyr@colostate.edu>
2025-05-05 17:52:43 -0600

    Revert "Eye prefab (Organ Key) and Level Design"
    This reverts commit 3f2fb08cdbedd6c07adcf70f0ba72cb35a74949b.

--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -23,7 +23,7 @@ namespace StarterAssets
 
 		[Space(10)]
 		[Tooltip("The height the player can jump")]
-		public float JumpHeight = 2.5f;
+		public float JumpHeight = 1.2f;
 		[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
 		public float Gravity = -15.0f;
 
